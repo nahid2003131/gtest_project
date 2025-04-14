b664be052dc89b3b0c9b14d90671a6659ccd5f8f
nahid2003131 <nahid2003131@gmail.com>
2025-04-14 23:17:09 +0600

    adding project

---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/nahid/Code/gtest-project
+# Build directory: /home/nahid/Code/gtest-project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(MyTests "/home/nahid/Code/gtest-project/build/testsuite")
+set_tests_properties(MyTests PROPERTIES  _BACKTRACE_TRIPLES "/home/nahid/Code/gtest-project/CMakeLists.txt;19;add_test;/home/nahid/Code/gtest-project/CMakeLists.txt;0;")
